b43650fde14b6dbb149797fbb4e2e8bb755316b8
Luqian <1791502895@qq.com>
2020-08-20 14:26:35 +0800

    1111

--- managesys-master/src/views/produceinfo/ProduceInfoManage.vue
+++ managesys-master/src/views/produceinfo/ProduceInfoManage.vue
@@ -1,16 +1,137 @@
 <template>
-    <div>s生产管理信息页面
-    	
-    </div>
-    
-    
+	<div>s生产管理信息页面
+		<el-row :gutter="20">
+			<el-col :span="16">
+				<div class="grid-content bg-purple">
+					
+				</div>
+			</el-col>
+			<el-col :span="8">
+				<div class="grid-content bg-purple">1111</div>
+			</el-col>
+		</el-row>
+		<!--表格布局-->
+		<el-table :data="tableData" height="300" border style="width: 100%">
+			<el-table-column fixed prop="table" label="" width="150" >
+			</el-table-column>
+			<el-table-column prop="eastC" label="东部C线" >
+			</el-table-column>
+			<el-table-column prop="eastD" label="东部D线" >
+			</el-table-column>
+			<el-table-column prop="westB" label="西部B线">
+			</el-table-column>
+			<el-table-column prop="westC" label="西部C线">
+			</el-table-column>
+			<el-table-column prop="sum" label="合计">
+			</el-table-column>
+			
+		</el-table>
+<!--日期选择按钮-->
+					<div class="block">
+						<!--<span class="demonstration">带快捷选项</span>-->
+						<el-date-picker v-model="value2" align="right" type="date" placeholder="日期选择" :picker-options="pickerOptions">
+						</el-date-picker>
+					</div>
+	</div>
+
 </template>
 <script>
-export default {
-    name: 'ProduceInfo'
-}
+	export default {
+		data() {
+			return {
+				//日期选择数据
+				pickerOptions: {
+					disabledData(time) {
+						return time.getTime() > Data.now();
+					},
+					shortcuts: [{
+						text: '今天',
+						onClick(picker) {
+							picker.$emit('pick', new Date());
+						}
+					}, {
+						text: '昨天',
+						onClick(picker) {
+							const date = new Date();
+							date.setTime(date.getTime() - 3600 * 1000 * 24);
+							picker.$emit('pick', date);
+
+						}
+					}, {
+						text: '一周前',
+						onClick(picker) {
+							const date = new Date();
+							date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+							picker.$emit('pick', date);
+						}
+					}]
+				},
+				//表格数据
+				tableData: [{
+						table: '目标台量',
+						eastC: '100',
+						eastD: '200',
+						westB:'200',
+						westC:'200',
+						sum:''
+					},
+					{
+						table: '实际台量',
+						eastC: '1',
+						eastD: '2',
+						westB:'3',
+						westC:'4',
+						sum:''
+					}, {
+						table: '实际效率',
+						eastC: '6',
+						eastD: '7',
+						westB:'8',
+						westC:'9',
+						sum:''
+					}
+				],
+				value1: '',
+				value2: '',
+
+			};
+		}
+
+		//  name: 'ProduceInfo'  
+	};
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped=>
-@import url("../../assets/scss/produce.scss");
+	@import url("../../assets/scss/produce.scss");
+	.el-row {
+		margin-bottom: 20px;
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
 	
+	.el-col {
+		border-radius: 4px;
+	}
+	
+	.bg-purple-dark {
+		background: #99a9bf;
+	}
+	
+	.bg-purple {
+		background: #d3dce6;
+	}
+	
+	.bg-purple-light {
+		background: #e5e9f2;
+	}
+	
+	.grid-content {
+		border-radius: 4px;
+		min-height: 36px;
+	}
+	
+	.row-bg {
+		padding: 10px 0;
+		background-color: #f9fafc;
+	}
 </style>
