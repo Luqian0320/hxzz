be42c135a1671d264e10cff592bf99ec93a52e93
Luqian <1791502895@qq.com>
2020-10-13 09:46:30 +0800

    four files

--- managesys-master/src/views/orderinfo/OrderInfoManage.vue
+++ managesys-master/src/views/orderinfo/OrderInfoManage.vue
@@ -1,8 +1,69 @@
 <template>
-    <div>L流转车信息页面</div>
+    <div class="ord">
+		<h1>流转车信息页面</h1>
+	<el-table :data="tableData" style="width: 100%">
+		<el-table-column prop="date" label="" width="180">
+		</el-table-column>
+		<el-table-column prop="drk" label="待入库" width="180">
+		</el-table-column>
+		<el-table-column prop="phl" label="待过线跑坏路">
+		</el-table-column>
+		<el-table-column prop="bhlz" label="BH流转">
+		</el-table-column>
+		<el-table-column prop="sjlz" label="实际流转">
+		</el-table-column>
+		<el-table-column prop="hj" label="合计">
+		</el-table-column>
+	</el-table>
+	<div class="btn">
+		 <el-button  round>新建/保存</el-button>
+		  <el-button  round>点击编辑</el-button>
+	</div>
+	</div>
+	 
 </template>
 <script>
 export default {
-    name: 'OrderInfoManage'
-}
+    data(){
+		return{
+				 value1: '',
+				tableData: [{
+					date: 'CN120S',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN180C',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN180S',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN185M',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}]
+			}
+		},
+		methods:{
+			
+		}
+	
+};
 </script>
+<style scoped>
+	@import url("../../assets/css/order.css");
+</style>
--- managesys-master/src/views/qualityinfo/QualityInfoManage.vue
+++ managesys-master/src/views/qualityinfo/QualityInfoManage.vue
@@ -1,8 +1,83 @@
 <template>
-    <div>z质量页面</div>
+    <div class="qual">
+	<h1>质量页面</h1>
+	<el-table :data="tableData" style="width: 100%">
+		<el-table-column prop="date" label="" width="180">
+		</el-table-column>
+		<el-table-column prop="xx" label="下线" width="180">
+		</el-table-column>
+		<el-table-column prop="jcx" label="检测线">
+		</el-table-column>
+		<el-table-column prop="ly" label="淋雨">
+		</el-table-column>
+		<el-table-column prop="care" label="Care线">
+		</el-table-column>
+		<el-table-column prop="dr" label="DR">
+		</el-table-column>
+	</el-table>
+	<div class="btn">
+		 <el-button  round>新建/保存</el-button>
+		  <el-button  round>点击编辑</el-button>
+		  </div>
+		  </div>
 </template>
 <script>
 export default {
-    name: 'QualityInfo'
-}
+    data(){
+		return{
+			tableData: [{
+				date: 'CN120S',
+				xx: '12',
+				jcx: '10',
+				ly:'9',
+				care:'8',
+				dr:''
+			},
+			{
+				date: 'CN180C',
+				xx: '12',
+				jcx: '10',
+				ly:'9',
+				care:'8',
+				dr:''
+			},
+			{
+				date: 'CN180S',
+				xx: '12',
+				jcx: '10',
+				ly:'9',
+				care:'8',
+				dr:''
+			},
+			{
+				date: 'CN185M',
+				xx: '12',
+				jcx: '10',
+				ly:'9',
+				care:'8',
+				dr:''
+			},
+			{
+				date: 'CN180S MCE',
+				xx: '12',
+				jcx: '10',
+				ly:'9',
+				care:'8',
+				dr:''
+			},
+			]
+		}
+	},
+	methods:{
+		
+	}
+};
 </script>
+<style>
+.btn{
+	display:flex;
+	margin-top:2.125rem;
+	margin-left: 50rem;
+	
+}	
+</style>
